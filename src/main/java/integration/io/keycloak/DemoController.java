c14ebd091cfd4d103a0169693a055ea8d415eef1
ahmed dacharoui <dachraouia193@gmail.com>
2025-02-11 15:25:58 +0100

    create and config jwtAuthConverter

--- src/main/java/integration/io/keycloak/DemoController.java
+++ src/main/java/integration/io/keycloak/DemoController.java
@@ -1,12 +1,15 @@
 package integration.io.keycloak;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/demo")
+
 public class DemoController {
 
     @GetMapping
@@ -14,13 +17,18 @@ public class DemoController {
         return "Hello, World!";
     }
 
+
     @GetMapping("/admin")
+    @PreAuthorize("hasRole('client_role')")
     public String admin() {
         return "Hello, Admin!";
     }
 
     @GetMapping("/user")
+    @PreAuthorize("hasRole('user_role')")
+    // hasRole('user_role') means that only users with the role user_role can access this endpoint.
     public String user() {
         return "Hello, User!";
     }
+
 }
--- src/main/java/integration/io/keycloak/SecurityConfig.java
+++ src/main/java/integration/io/keycloak/SecurityConfig.java
@@ -3,6 +3,7 @@ package integration.io.keycloak;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,7 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
